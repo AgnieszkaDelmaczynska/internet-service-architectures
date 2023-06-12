6bc652fdcc64a85a5c4c57725c61c39e98ba4778
Agnieszka Marika Delmaczyńska <105732925+AgnieszkaDelmaczynska@users.noreply.github.com>
2023-06-12 15:16:20 +0200

    Add lab3 gateway

---
+++ AUI_lab3_gateway/src/main/java/pg/AUI_lab3_gateway/AuiLab1Application.java
@@ -0,0 +1,53 @@
+package pg.AUI_lab3_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@SpringBootApplication
+public class AuiLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuiLab1Application.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				.route("films", r -> r
+						.host("localhost:8080")
+						.and()
+						.path("/api/films/{title}", "/api/films")
+						.uri("http://localhost:8081"))
+				.route("cinemas", r -> r
+						.host("localhost:8080")
+						.and()
+						.path("/api/cinemas", "/api/cinemas/**", "/api/films/{title}/cinemas", "/api/films/{title}/cinemas/**")
+						.uri("http://localhost:8082"))
+				.build();
+	}
+
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+
+		final CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+		corsConfig.setMaxAge(3600L);
+		corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
+		corsConfig.addAllowedHeader("*");
+
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+
+		return new CorsWebFilter(source);
+	}
+}
---
+++ AUI_lab3_gateway/src/test/java/pg/AUI_lab3_gateway/AuiLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pg.AUI_lab3_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuiLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
